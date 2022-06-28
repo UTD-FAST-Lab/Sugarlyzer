41254ecbe21006e12a2484dd1915b42a5da3b918
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-06-28 17:18:27 -0500

    Added Alarm tests

--- tests/sugarlyzer/models/test_Alarm.py
+++ tests/sugarlyzer/models/test_Alarm.py
@@ -1,5 +1,32 @@
-from unittest import TestCase
+import re
+
+from hypothesis import given
+from hypothesis.strategies import text, integers, composite, from_regex
+
+from src.sugarlyzer.models.Alarm import Alarm
 
 
-class TestAlarm(TestCase):
-    pass
+@composite
+def build_alarm(draw,
+                strings=text(),
+                nums=integers()):
+    return Alarm(file = draw(strings),
+                 start_line = draw(nums),
+                 end_line = draw(nums),
+                 alarm_type = draw(strings),
+                 message = draw(strings))
+
+
+@given(build_alarm())
+def test_alarm_sanitize_length_invariant(alarm: Alarm):
+    assert (len(alarm.sanitized_message) <= len(alarm.message))
+
+
+@given(build_alarm(strings=from_regex(r" '\S+'")))
+def test_alarm_sanitize_no_var_names_invariant(alarm: Alarm):
+    assert (not re.match(r" '\S+'", alarm.sanitized_message.replace("'x'", "")))
+
+
+@given(build_alarm(strings=from_regex(r" \[.*\]$")))
+def test_alarm_sanitize_no_brackets(alarm: Alarm):
+    assert (not re.match(r" \[.*\]$", alarm.sanitized_message))
