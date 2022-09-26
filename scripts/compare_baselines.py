0d03d1fb9f4378af5c5d3965b25fe7380bd02ac1
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-09-26 16:01:20 -0500

    Attempt to fix output

--- scripts/compare_baselines.py
+++ scripts/compare_baselines.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 import logging
 import re
@@ -5,10 +6,12 @@ import re
 from z3 import Solver, And, Or, Not, Bool
 
 import argparse
+
 p = argparse.ArgumentParser()
 p.add_argument('baselines', help='The file containing the baseline alarms.')
 p.add_argument('experimental_results', help='The file containing the experimental results.')
 args = p.parse_args()
+
 
 def main():
     with open(args.baselines) as f:
@@ -17,23 +20,37 @@ def main():
     with open(args.experimental_results) as f:
         experimental_results = json.load(f)
 
+    # Fix ids:
+    id = 0
+    for b in itertools.chain(baselines, experimental_results):
+        b['id'] = id
+        id += 1
+
+    rows = []
     ## Transformations
     for e in experimental_results:
         if (toks := e['original_line'].split(':'))[0] != toks[1]:
             raise ValueError("Need to implement handling for original line ranges.")
         else:
             e['original_line'] = int(toks[0])
+        print('\t'.join(e.values()))
 
+    results = []
     for b in baselines:
-        matches = (e for e in experimental_results if e['original_line'] == b['input_line'] and \
-                   e['sanitized_message'] == b['message'] and \
-                   e['input_file'].split('.')[0] == b['input_file'].split('.')[0])
+        matches = [e for e in experimental_results if e['original_line'] == b['input_line'] and \
+                   e['input_file'].split('.')[0] == b['input_file'].split('.')[0]]
+        exact_matches = [m['id'] for m in matches if b['message'] == m['sanitized_message']]
+        partial_matches = [m['id'] for m in matches if m not in exact_matches]
+        vals = b.values()
+
+        vals.extend([exact_matches, partial_matches, len(exact_matches), len(partial_matches)])
+        print('\t'.join(vals))
 
 
-        for m in matches:
-            m['presence_condition'] = re.sub("\(defined (.*?)\)", "(\1)", m['presence_condition'])
-            print(m['presence_condition'])
+
+
             # Now, to construct the Z3 stuff *rolls sleeves up*
+
 
 if __name__ == '__main__':
     main()
