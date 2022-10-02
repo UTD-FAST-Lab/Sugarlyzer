78c517c114fcddd552eb4a8170a95b8b8c257bdc
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-10-02 01:01:36 -0500

    Fix output

--- src/sugarlyzer/models/Alarm.py
+++ src/sugarlyzer/models/Alarm.py
@@ -83,7 +83,7 @@ class Alarm:
             "sanitized_message": lambda: self.sanitized_message,
             "presence_condition": lambda: self.presence_condition,
             "feasible": lambda: self.feasible,
-            "configuration": lambda: self.model
+            "configuration": lambda: str(self.model)
         }
 
         result = {}
