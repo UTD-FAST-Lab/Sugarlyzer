4f15a78631b27c59bb9da08abe2fc52b0a3bbfcb
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-10-06 16:09:03 -0500

    fix circular import

--- src/sugarlyzer/Tester.py
+++ src/sugarlyzer/Tester.py
@@ -197,34 +197,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-def get_all_macros(fpa):
-    ff = open(fpa, 'r')
-    lines = ff.read().split('\n')
-    defs = []
-    for l in lines:
-        if '#if' in l:
-            m = re.match(' *#ifdef *([a-zA-Z0-9_]+).*', l)
-            if m:
-                v = m.group(1)
-                if v not in defs:
-                    defs.append(v)
-                continue
-            m = re.match(' *#ifndef *([a-zA-Z0-9_]+).*', l)
-            if m:
-                v = m.group(1)
-                if v not in defs:
-                    defs.append(v)
-                continue
-            m = re.match(' *#if *([a-zA-Z0-9_]+).*', l)
-            if m:
-                v = m.group(1)
-                if v not in defs:
-                    defs.append(v)
-                continue
-            ds = re.findall(r'defined *([a-zA-Z0-9_]+?)', l)
-            for d in ds:
-                if d not in defs:
-                    defs.append(d)
-    return defs
