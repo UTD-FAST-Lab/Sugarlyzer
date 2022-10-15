89770f31a8d93cc85597285f1c430e71deedc3ee
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-10-15 18:16:17 -0500

    Use named temp file

--- src/sugarlyzer/analyses/AbstractTool.py
+++ src/sugarlyzer/analyses/AbstractTool.py
@@ -34,7 +34,7 @@ class AbstractTool(ABC):
         :return: A collection of alarms.
         """
         if user_defined_space is not None:
-            with tempfile.TemporaryFile('w') as uds:
+            with tempfile.NamedTemporaryFile('w') as uds:
                 uds.write(user_defined_space)
                 if included_files is None:
                     included_files = []
