93df947b1886e04d9a34c9a79448dc5cd4ec80ec
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-10-14 19:08:08 -0500

    Logging

--- src/sugarlyzer/util/decorators.py
+++ src/sugarlyzer/util/decorators.py
@@ -4,9 +4,13 @@ from typing import Callable
 logger = logging.getLogger(__name__)
 
 
-def log_all_params_and_return(func: Callable):
-    def wrapper(*args, **kwargs):
-        ret = func(*args, **kwargs)
-        logger.debug(f"Called a function {str(func)} with args {args} and kwargs {kwargs}, and it returned {ret}")
-        return ret
-    return wrapper
+def log_all_params_and_return(logger_func=logger.debug):
+    def decorator(func: Callable):
+        def wrapper(*args, **kwargs):
+            ret = func(*args, **kwargs)
+            logger_func(f"Called a function {str(func)} with args {args} and kwargs {kwargs}, and it returned {ret}")
+            return ret
+
+        return wrapper
+
+    return decorator
