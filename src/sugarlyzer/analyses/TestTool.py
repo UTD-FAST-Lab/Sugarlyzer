4393257d9eb3fbe3cca8ca94b477ad074236afdc
pattersonz <pattersonz.se@gmail.com>
2022-10-22 14:38:43 -0500

    changed references of static cond to look for a funciton, made testReader for no analysis runs

--- src/sugarlyzer/analyses/TestTool.py
+++ src/sugarlyzer/analyses/TestTool.py
@@ -1,11 +1,16 @@
 from src.sugarlyzer.analyses.AbstractTool import AbstractTool
+from pathlib import Path
+from typing import Iterable, Optional
+from src.sugarlyzer.readers.TestReader import TestReader
 
 
 class TestTool(AbstractTool):
 
     def __init__(self):
-        self.reader = None
-
-    def analyze(self, file: str) -> str:
+        super().__init__(TestReader(), keep_mem=True, make_main=True, remove_errors=False)
+    def analyze(self, file: Path,
+                included_dirs: Iterable[Path] = None,
+                included_files: Iterable[Path] = None,
+                command_line_defs: Iterable[str] = None):
         print(f"Analyzing {file}")
-        return file
+        yield file
