01a8924ce7f390e9a594576432dbe07ffd175f75
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-10-29 09:21:58 +0200

    Handle exception

--- src/sugarlyzer/util/MacroDiscoveryPreprocessor.py
+++ src/sugarlyzer/util/MacroDiscoveryPreprocessor.py
@@ -1,8 +1,37 @@
+import logging
+from argparse import Action
+
 import pcpp
 from pcpp import OutputDirective
 
+logger = logging.getLogger(__name__)
+
 
 class MacroDiscoveryPreprocessor(pcpp.Preprocessor):
+
+    def on_include_not_found(self, is_malformed, is_system_include, curdir, includepath):
+        """Called when a #include wasn't found.
+
+        Raise OutputDirective to pass through or remove, else return
+        a suitable path. Remember that Preprocessor.add_path() lets you add search paths.
+
+        The default calls ``self.on_error()`` with a suitable error message about the
+        include file not found if ``is_malformed`` is False, else a suitable error
+        message about a malformed #include, and in both cases raises OutputDirective
+        (pass through).
+        """
+        try:
+            if is_malformed:
+                self.on_error(self.lastdirective.source, self.lastdirective.lineno,
+                                  "Malformed #include statement: %s" % includepath)
+            else:
+                self.on_error(self.lastdirective.source, self.lastdirective.lineno,
+                              "Include file '%s' not found" % includepath)
+        except AttributeError as ae:
+            logger.exception("Exception encountered while trying to raise error.")
+        finally:
+            raise OutputDirective(Action.IgnoreAndPassThrough)
+
     def on_directive_handle(self, directive, toks, ifpassthru, precedingtoks):
         if directive.value == 'define':
             self.defined.extend([t.value for t in toks])
@@ -13,7 +42,6 @@ class MacroDiscoveryPreprocessor(pcpp.Preprocessor):
         self.defined = []
         self.__collected = collected
         super(MacroDiscoveryPreprocessor, self).__init__(*args, **kwargs)
-
 
     @property
     def collected(self):
