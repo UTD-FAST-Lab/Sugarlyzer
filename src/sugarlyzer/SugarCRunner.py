5eca28115393c6272a0ff79c26c587e9d4064602
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-11-07 17:11:10 -0600

    Make paths relative

--- src/sugarlyzer/SugarCRunner.py
+++ src/sugarlyzer/SugarCRunner.py
@@ -175,10 +175,16 @@ def desugar_file(file_to_desugar: Path,
         outfile.write(recommended_space)
     included_files.append(outfile.name)
 
+    def relative_to_wrapper(i, fil: Path):
+        try:
+            return Path(i).relative_to(fil.parent)
+        except ValueError:
+            return Path(i)
+
     included_files = list(itertools.chain(*zip(['-include'] * len(included_files),
-                                               (Path(i).relative_to(file_to_desugar.parent) for i in included_files))))
+                                               (relative_to_wrapper(i, file_to_desugar.parent) for i in included_files))))
     included_directories = list(itertools.chain(*zip(['-I'] * len(included_directories),
-                                                     (Path(i).relative_to(file_to_desugar.parent) for i in included_directories))))
+                                                     (relative_to_wrapper(i, file_to_desugar.parent) for i in included_directories))))
     commandline_args = []
     commandline_args = ['-nostdinc', *commandline_args] if no_stdlibs else commandline_args
     commandline_args = ['-keep-mem', *commandline_args] if keep_mem else commandline_args
