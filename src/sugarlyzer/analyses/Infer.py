b97ed5d55ddff0650ff1664ad7fbce0c415ecd84
Austin Mordahl <austin.mordahl@utdallas.edu>
2023-11-10 13:55:18 -0600

    Add cpu time to infer

--- src/sugarlyzer/analyses/Infer.py
+++ src/sugarlyzer/analyses/Infer.py
@@ -4,6 +4,7 @@ import subprocess
 import tempfile
 from pathlib import Path
 from typing import Iterable, Optional
+import re
 
 from src.sugarlyzer.analyses.AbstractTool import AbstractTool
 import os
@@ -30,15 +31,21 @@ class Infer(AbstractTool):
             command_line_defs = []
 
         output_location = tempfile.mkdtemp()
-        cmd = ["ulimit -v 100000000;", "infer", "--pulse-only", '-o', output_location, '--', "clang",
+        cmd = ["ulimit -v 100000000;", "time", "infer", "--pulse-only", '-o', output_location, '--', "clang",
                *list(itertools.chain(*zip(itertools.cycle(["-I"]), included_dirs))),
                *list(itertools.chain(*zip(itertools.cycle(["--include"]), included_files))),
                *command_line_defs,
                "-nostdinc", "-c", file.absolute()]
         logger.debug(f"Running cmd {cmd}")
-        ps = subprocess.run(" ".join([str(s) for s in cmd]), stderr=subprocess.STDOUT, stdout=subprocess.PIPE, text=True, shell=True, executable='/bin/bash')
+        ps = subprocess.run(" ".join([str(s) for s in cmd]), text=True, shell=True, executable='/bin/bash')
         if (ps.returncode != 0):
             logger.warning(f"Running infer on file {str(file)} with command {' '.join(str(s) for s in cmd)} potentially failed (exit code {ps.returncode}).")
             logger.warning(ps.stdout)
+        times = " ".join(ps.stderr.split('\n')[-10:])
+        usr_time_match = re.search(r"user\\t([\d\.]*)m([\d\.]*)s", times)
+        usr_time = float(usr_time_match.group(1)) * 60 + float(usr_time_match.group(1))
+        sys_time_match = re.search(r"sys\\t([\d\.]*)m([\d\.]*)s", times)
+        sys_time = float(sys_time_match.group(1)) * 60 + float(sys_time_match.group(1))
+        logger.info(f"CPU time to analyze {file} was {usr_time + sys_time}")
         report = os.path.join(output_location,'report.json')
         yield report
