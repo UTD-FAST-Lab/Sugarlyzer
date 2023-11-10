42e6257996164771c59dc817c1bf2fe1470714ce
Austin Mordahl <austin.mordahl@utdallas.edu>
2023-11-10 16:25:37 -0600

    I don't know anymore, just trying stuff

--- src/sugarlyzer/SugarCRunner.py
+++ src/sugarlyzer/SugarCRunner.py
@@ -259,9 +259,9 @@ def run_sugarc(cmd_str, file_to_desugar: Path, desugared_output: Path, log_file)
             times = '\n'.join(ps.stderr.split('\n')[-10:])
             logger.info("times: " + times)
             try:
-                usr_time_match = re.search(r"user.*?([\d\.]*)m([\d\.]*)s", times)
+                usr_time_match = re.search("user.*?([\\d.]*)m([\\d.]*)s", times)
                 usr_time = float(usr_time_match.group(1)) * 60 + float(usr_time_match.group(1))
-                sys_time_match = re.search(r"sys.*?([\d\.]*)m([\d\.]*)s", times)
+                sys_time_match = re.search("sys.*?([\\d.]*)m([\\d.]*)s", times)
                 sys_time = float(sys_time_match.group(1)) * 60 + float(sys_time_match.group(1))
                 logger.info("end of stderr: " + str(str(ps.stderr, 'UTF-8').split('\n')[-10:]))
             except AttributeError as ae:
