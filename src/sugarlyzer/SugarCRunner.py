64437053789f10d770a194919cd0ba5732dd7303
Austin Mordahl <austin.mordahl@utdallas.edu>
2023-11-10 15:23:00 -0600

    exception handling

--- src/sugarlyzer/SugarCRunner.py
+++ src/sugarlyzer/SugarCRunner.py
@@ -256,11 +256,16 @@ def run_sugarc(cmd_str, file_to_desugar: Path, desugared_output: Path, log_file)
             ps = subprocess.run(cmd_str, capture_output=True, shell=True, executable='/bin/bash')
             times = ' '.join(str(ps.stderr, 'UTF-8').split('\n')[-10:])
             logger.info("times: " + times)
-            usr_time_match = re.search(r"user\\t([\d\.]*)m([\d\.]*)s", times)
-            usr_time = float(usr_time_match.group(1)) * 60 + float(usr_time_match.group(1))
-            sys_time_match = re.search(r"sys\\t([\d\.]*)m([\d\.]*)s", times)
-            sys_time = float(sys_time_match.group(1)) * 60 + float(sys_time_match.group(1))
-            logger.info("end of stderr: " + str(str(ps.stderr, 'UTF-8').split('\n')[-10:]))
+            try:
+                usr_time_match = re.search(r"user\\t([\d\.]*)m([\d\.]*)s", times)
+                usr_time = float(usr_time_match.group(1)) * 60 + float(usr_time_match.group(1))
+                sys_time_match = re.search(r"sys\\t([\d\.]*)m([\d\.]*)s", times)
+                sys_time = float(sys_time_match.group(1)) * 60 + float(sys_time_match.group(1))
+                logger.info("end of stderr: " + str(str(ps.stderr, 'UTF-8').split('\n')[-10:]))
+            except AttributeError as ae:
+                logger.info(f"Couldn't match {times}")
+                usr_time = 0
+                sys_time = 0
 
             with open(desugared_output, 'wb') as f:
                 f.write(ps.stdout)
