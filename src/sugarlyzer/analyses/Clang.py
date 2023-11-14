b6567e2bbd3cb286b9d00f1e7038c2dad0f5bc4d
Austin Mordahl <austin.mordahl@utdallas.edu>
2023-11-14 01:48:42 -0600

    remove limit, add time to phasar

--- src/sugarlyzer/analyses/Clang.py
+++ src/sugarlyzer/analyses/Clang.py
@@ -34,7 +34,7 @@ class Clang(AbstractTool):
             included_files = []
 
         output_location = tempfile.mkdtemp()
-        cmd = ["ulimit -v 100000000;", "time", "clang-11", '--analyze', "-Xanalyzer", "-analyzer-output=text",
+        cmd = ["time", "clang-11", '--analyze', "-Xanalyzer", "-analyzer-output=text",
                *list(itertools.chain(*zip(itertools.cycle(["-I"]), included_dirs))),
                *list(itertools.chain(*zip(itertools.cycle(["--include"]), included_files))),
                *command_line_defs,
--- src/sugarlyzer/analyses/Phasar.py
+++ src/sugarlyzer/analyses/Phasar.py
@@ -3,7 +3,7 @@ import logging
 import subprocess
 import tempfile
 from pathlib import Path
-from typing import Iterable, Optional
+from typing import Iterable, Optional, re
 
 from src.sugarlyzer.analyses.AbstractTool import AbstractTool
 import os
@@ -29,23 +29,42 @@ class Phasar(AbstractTool):
         output_location = tempfile.mkdtemp()
         #create ll file
         llFile = os.path.join(output_location,str(file)[:-2]+'.ll')
-        cmd = ['clang-12','-emit-llvm','-S','-fno-discard-value-names','-c','-g',
+        cmd = ['time', 'clang-12','-emit-llvm','-S','-fno-discard-value-names','-c','-g',
                *list(itertools.chain(*zip(itertools.cycle(["-I"]), included_dirs))),
                *list(itertools.chain(*zip(itertools.cycle(["--include"]), included_files))),
                *command_line_defs,
                "-nostdinc", "-c", file.absolute(), '-o', llFile]
         logger.info(f"Running cmd {cmd}")
-        ps = subprocess.run(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, text=True)
+        ps = subprocess.run(" ".join(str(s) for s in cmd), text=True)
         if (ps.returncode != 0) or ("error" in ps.stdout.lower()):
             logger.warning(f"Running clang on file {str(file)} potentially failed.")
-            logger.warning(ps.stdout)
+            logger.warning(ps.stderr)
+
+        times = " ".join(ps.stderr.split('\n')[-10:])
+        usr_time_match = re.search("user.*?([\\d.]*)m([\\d.]*)s", times)
+        usr_time = float(usr_time_match.group(1)) * 60 + float(usr_time_match.group(2))
+        logger.info("Usr time for clang is " + str(usr_time))
+        sys_time_match = re.search("sys.*?([\\d.]*)m([\\d.]*)s", times)
+        sys_time = float(sys_time_match.group(1)) * 60 + float(sys_time_match.group(2))
+        logger.info("Sys time for clang is " + str(sys_time))
+
         #run phasar on ll
-        cmd = ['/phasar/build/tools/phasar-llvm/phasar-llvm','-D','IFDSUninitializedVariables','-m',llFile,'-O',output_location]
+        cmd = ['time', '/phasar/build/tools/phasar-llvm/phasar-llvm','-D','IFDSUninitializedVariables','-m',llFile,'-O',output_location]
         logger.info(f"Running cmd {cmd}")
-        ps = subprocess.run(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, text=True)
+        ps = subprocess.run(cmd, capture_output=True, text=True, executable="/bin/bash")
         if (ps.returncode != 0) or ("error" in ps.stdout.lower()):
             logger.warning(f"Running phasar on file {str(file)} potentially failed.")
-            logger.warning(ps.stdout)
+            logger.warning(ps.stderr)
+
+        times = " ".join(ps.stderr.split('\n')[-10:])
+        usr_time_match = re.search("user.*?([\\d.]*)m([\\d.]*)s", times)
+        usr_time += float(usr_time_match.group(1)) * 60 + float(usr_time_match.group(2))
+        logger.info("Usr time for phasar is " + str(usr_time))
+        sys_time_match = re.search("sys.*?([\\d.]*)m([\\d.]*)s", times)
+        sys_time += float(sys_time_match.group(1)) * 60 + float(sys_time_match.group(2))
+        logger.info("Sys time for phasar is " + str(sys_time))
+        logger.info(f"CPU time to analyze {file} was {usr_time + sys_time}")
+
         for root, dirs, files in os.walk(output_location):
             for f in files:
                 if f.startswith("psr-report") and f.endswith(".txt"):
