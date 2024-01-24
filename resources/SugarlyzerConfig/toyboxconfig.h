b048d56859421372dfc750cf57b233c63269f083
Austin Mordahl <austin.mordahl@utdallas.edu>
2024-01-24 14:17:12 -0600

    Add SugarlyzerConfig locally

---
+++ resources/SugarlyzerConfig/toyboxconfig.h
@@ -0,0 +1,314 @@
+#define USE_TOYBOX_CONTAINER(...)
+#define USE_TOYBOX_FIFREEZE(...) __VA_ARGS__
+#define USE_TOYBOX_ICONV(...) __VA_ARGS__
+#define USE_TOYBOX_FALLOCATE(...) __VA_ARGS__
+#define USE_TOYBOX_UTMPX(...) __VA_ARGS__
+#define USE_TOYBOX_SHADOW(...) __VA_ARGS__
+#define USE_TOYBOX_ON_ANDROID(...)
+#define USE_TOYBOX_ANDROID_SCHEDPOLICY(...)
+#define USE_TOYBOX_FORK(...) __VA_ARGS__
+#define USE_TOYBOX_PRLIMIT(...)
+#define USE_BASENAME(...) __VA_ARGS__
+#define USE_CAL(...) __VA_ARGS__
+#define USE_CAT(...) __VA_ARGS__
+#define USE_CAT_V(...)
+#define USE_CATV(...) __VA_ARGS__
+#define USE_CHGRP(...) __VA_ARGS__
+#define USE_CHOWN(...) __VA_ARGS__
+#define USE_CHMOD(...) __VA_ARGS__
+#define USE_CKSUM(...) __VA_ARGS__
+#define USE_CMP(...) __VA_ARGS__
+#define USE_COMM(...) __VA_ARGS__
+#define USE_CP(...) __VA_ARGS__
+#define USE_CP_PRESERVE(...) __VA_ARGS__
+#define USE_MV(...) __VA_ARGS__
+#define USE_INSTALL(...) __VA_ARGS__
+#define USE_CPIO(...) __VA_ARGS__
+#define USE_CUT(...) __VA_ARGS__
+#define USE_DATE(...) __VA_ARGS__
+#define USE_DF(...) __VA_ARGS__
+#define USE_DIRNAME(...) __VA_ARGS__
+#define USE_DU(...) __VA_ARGS__
+#define USE_ECHO(...) __VA_ARGS__
+#define USE_ENV(...) __VA_ARGS__
+#define USE_EXPAND(...) __VA_ARGS__
+#define USE_FALSE(...) __VA_ARGS__
+#define USE_FILE(...) __VA_ARGS__
+#define USE_FIND(...) __VA_ARGS__
+#define USE_GREP(...) __VA_ARGS__
+#define USE_EGREP(...) __VA_ARGS__
+#define USE_FGREP(...) __VA_ARGS__
+#define USE_HEAD(...) __VA_ARGS__
+#define USE_ID(...) __VA_ARGS__
+#define USE_ID_Z(...)
+#define USE_GROUPS(...) __VA_ARGS__
+#define USE_LOGNAME(...) __VA_ARGS__
+#define USE_WHOAMI(...) __VA_ARGS__
+#define USE_KILL(...) __VA_ARGS__
+#define USE_KILLALL5(...) __VA_ARGS__
+#define USE_LINK(...) __VA_ARGS__
+#define USE_LN(...) __VA_ARGS__
+#define USE_LS(...) __VA_ARGS__
+#define USE_LS_COLOR(...) __VA_ARGS__
+#define USE_MKDIR(...) __VA_ARGS__
+#define USE_MKDIR_Z(...)
+#define USE_MKFIFO(...) __VA_ARGS__
+#define USE_MKFIFO_Z(...)
+#define USE_NICE(...) __VA_ARGS__
+#define USE_NL(...) __VA_ARGS__
+#define USE_NOHUP(...) __VA_ARGS__
+#define USE_OD(...) __VA_ARGS__
+#define USE_PASTE(...) __VA_ARGS__
+#define USE_PATCH(...) __VA_ARGS__
+#define USE_PRINTF(...) __VA_ARGS__
+#define USE_PS(...) __VA_ARGS__
+#define USE_TOP(...) __VA_ARGS__
+#define USE_IOTOP(...) __VA_ARGS__
+#define USE_PGREP(...) __VA_ARGS__
+#define USE_PKILL(...) __VA_ARGS__
+#define USE_PWD(...) __VA_ARGS__
+#define USE_RENICE(...) __VA_ARGS__
+#define USE_RM(...) __VA_ARGS__
+#define USE_RMDIR(...) __VA_ARGS__
+#define USE_SED(...) __VA_ARGS__
+#define USE_SLEEP(...) __VA_ARGS__
+#define USE_SLEEP_FLOAT(...) __VA_ARGS__
+#define USE_SORT(...) __VA_ARGS__
+#define USE_SORT_BIG(...) __VA_ARGS__
+#define USE_SORT_FLOAT(...) __VA_ARGS__
+#define USE_SPLIT(...) __VA_ARGS__
+#define USE_STRINGS(...) __VA_ARGS__
+#define USE_TAIL(...) __VA_ARGS__
+#define USE_TAIL_SEEK(...) __VA_ARGS__
+#define USE_TEE(...) __VA_ARGS__
+#define USE_TIME(...) __VA_ARGS__
+#define USE_TOUCH(...) __VA_ARGS__
+#define USE_TRUE(...) __VA_ARGS__
+#define USE_TTY(...) __VA_ARGS__
+#define USE_ULIMIT(...)
+#define USE_UNAME(...) __VA_ARGS__
+#define USE_UNIQ(...) __VA_ARGS__
+#define USE_UNLINK(...) __VA_ARGS__
+#define USE_UUDECODE(...) __VA_ARGS__
+#define USE_UUENCODE(...) __VA_ARGS__
+#define USE_WC(...) __VA_ARGS__
+#define USE_WHO(...) __VA_ARGS__
+#define USE_XARGS(...) __VA_ARGS__
+#define USE_XARGS_PEDANTIC(...)
+#define USE_ARP(...)
+#define USE_ARPING(...)
+#define USE_BOOTCHARTD(...)
+#define USE_BRCTL(...)
+#define USE_COMPRESS(...)
+#define USE_DECOMPRESS(...)
+#define USE_CROND(...)
+#define USE_CRONTAB(...)
+#define USE_DD(...)
+#define USE_DHCP6(...)
+#define USE_DHCP(...)
+#define USE_DHCPD(...)
+#define USE_DEBUG_DHCP(...)
+#define USE_DIFF(...)
+#define USE_DUMPLEASES(...)
+#define USE_EXPR(...)
+#define USE_FDISK(...)
+#define USE_FOLD(...)
+#define USE_FSCK(...)
+#define USE_GETFATTR(...)
+#define USE_GETTY(...)
+#define USE_GROUPADD(...)
+#define USE_GROUPDEL(...)
+#define USE_GZIP(...)
+#define USE_GUNZIP(...)
+#define USE_ZCAT(...)
+#define USE_HOST(...)
+#define USE_ICONV(...)
+#define USE_INIT(...)
+#define USE_IP(...)
+#define USE_IPCRM(...)
+#define USE_IPCS(...)
+#define USE_KLOGD(...)
+#define USE_KLOGD_SOURCE_RING_BUFFER(...)
+#define USE_LAST(...)
+#define USE_LOGGER(...)
+#define USE_LSOF(...)
+#define USE_MDEV(...)
+#define USE_MDEV_CONF(...)
+#define USE_MKE2FS(...)
+#define USE_MKE2FS_JOURNAL(...)
+#define USE_MKE2FS_GEN(...)
+#define USE_MKE2FS_LABEL(...)
+#define USE_MKE2FS_EXTENDED(...)
+#define USE_MODPROBE(...)
+#define USE_MORE(...)
+#define USE_OPENVT(...)
+#define USE_DEALLOCVT(...)
+#define USE_PING(...)
+#define USE_ROUTE(...)
+#define USE_SETFATTR(...)
+#define USE_SH(...)
+#define USE_CD(...)
+#define USE_EXIT(...)
+#define USE_SULOGIN(...)
+#define USE_SYSLOGD(...)
+#define USE_TAR(...)
+#define USE_TCPSVD(...)
+#define USE_TELNET(...)
+#define USE_TELNETD(...)
+#define USE_TEST(...)
+#define USE_TFTP(...)
+#define USE_TFTPD(...)
+#define USE_TRACEROUTE(...)
+#define USE_TR(...)
+#define USE_USERADD(...)
+#define USE_USERDEL(...)
+#define USE_VI(...)
+#define USE_WATCH(...)
+#define USE_WGET(...)
+#define USE_XZCAT(...)
+#define USE_ACPI(...) __VA_ARGS__
+#define USE_ASCII(...)
+#define USE_BASE64(...) __VA_ARGS__
+#define USE_BLKID(...) __VA_ARGS__
+#define USE_FSTYPE(...) __VA_ARGS__
+#define USE_BLOCKDEV(...) __VA_ARGS__
+#define USE_BUNZIP2(...) __VA_ARGS__
+#define USE_BZCAT(...) __VA_ARGS__
+#define USE_CHCON(...)
+#define USE_CHROOT(...) __VA_ARGS__
+#define USE_CHRT(...) __VA_ARGS__
+#define USE_CHVT(...) __VA_ARGS__
+#define USE_CLEAR(...) __VA_ARGS__
+#define USE_COUNT(...) __VA_ARGS__
+#define USE_DOS2UNIX(...) __VA_ARGS__
+#define USE_UNIX2DOS(...) __VA_ARGS__
+#define USE_EJECT(...) __VA_ARGS__
+#define USE_FACTOR(...) __VA_ARGS__
+#define USE_FALLOCATE(...) __VA_ARGS__
+#define USE_FLOCK(...) __VA_ARGS__
+#define USE_FREE(...) __VA_ARGS__
+#define USE_FREERAMDISK(...) __VA_ARGS__
+#define USE_FSFREEZE(...) __VA_ARGS__
+#define USE_FSYNC(...) __VA_ARGS__
+#define USE_HELP(...) __VA_ARGS__
+#define USE_HELP_EXTRAS(...) __VA_ARGS__
+#define USE_HEXEDIT(...) __VA_ARGS__
+#define USE_HWCLOCK(...) __VA_ARGS__
+#define USE_INOTIFYD(...) __VA_ARGS__
+#define USE_INSMOD(...) __VA_ARGS__
+#define USE_IONICE(...) __VA_ARGS__
+#define USE_IORENICE(...) __VA_ARGS__
+#define USE_LOGIN(...) __VA_ARGS__
+#define USE_LOSETUP(...) __VA_ARGS__
+#define USE_LSATTR(...) __VA_ARGS__
+#define USE_CHATTR(...) __VA_ARGS__
+#define USE_LSMOD(...) __VA_ARGS__
+#define USE_LSPCI(...) __VA_ARGS__
+#define USE_LSPCI_TEXT(...) __VA_ARGS__
+#define USE_LSUSB(...) __VA_ARGS__
+#define USE_MAKEDEVS(...) __VA_ARGS__
+#define USE_MIX(...) __VA_ARGS__
+#define USE_MKPASSWD(...) __VA_ARGS__
+#define USE_MKSWAP(...) __VA_ARGS__
+#define USE_MODINFO(...) __VA_ARGS__
+#define USE_MOUNTPOINT(...) __VA_ARGS__
+#define USE_NBD_CLIENT(...) __VA_ARGS__
+#define USE_UNSHARE(...)
+#define USE_NSENTER(...)
+#define USE_ONEIT(...) __VA_ARGS__
+#define USE_PARTPROBE(...) __VA_ARGS__
+#define USE_PIVOT_ROOT(...) __VA_ARGS__
+#define USE_PMAP(...) __VA_ARGS__
+#define USE_PRINTENV(...) __VA_ARGS__
+#define USE_PWDX(...) __VA_ARGS__
+#define USE_READAHEAD(...) __VA_ARGS__
+#define USE_READLINK(...) __VA_ARGS__
+#define USE_REALPATH(...) __VA_ARGS__
+#define USE_REBOOT(...) __VA_ARGS__
+#define USE_RESET(...) __VA_ARGS__
+#define USE_REV(...) __VA_ARGS__
+#define USE_RMMOD(...) __VA_ARGS__
+#define USE_SETSID(...) __VA_ARGS__
+#define USE_SHRED(...) __VA_ARGS__
+#define USE_STAT(...) __VA_ARGS__
+#define USE_SWAPOFF(...) __VA_ARGS__
+#define USE_SWAPON(...) __VA_ARGS__
+#define USE_SWITCH_ROOT(...) __VA_ARGS__
+#define USE_SYSCTL(...) __VA_ARGS__
+#define USE_TAC(...) __VA_ARGS__
+#define USE_NPROC(...) __VA_ARGS__
+#define USE_TASKSET(...) __VA_ARGS__
+#define USE_TIMEOUT(...) __VA_ARGS__
+#define USE_TRUNCATE(...) __VA_ARGS__
+#define USE_UPTIME(...) __VA_ARGS__
+#define USE_USLEEP(...) __VA_ARGS__
+#define USE_VCONFIG(...) __VA_ARGS__
+#define USE_VMSTAT(...) __VA_ARGS__
+#define USE_W(...) __VA_ARGS__
+#define USE_WHICH(...) __VA_ARGS__
+#define USE_XXD(...) __VA_ARGS__
+#define USE_YES(...) __VA_ARGS__
+#define USE_FTPGET(...) __VA_ARGS__
+#define USE_FTPPUT(...) __VA_ARGS__
+#define USE_IFCONFIG(...) __VA_ARGS__
+#define USE_MICROCOM(...) __VA_ARGS__
+#define USE_NETCAT(...) __VA_ARGS__
+#define USE_NETCAT_LISTEN(...) __VA_ARGS__
+#define USE_NETSTAT(...) __VA_ARGS__
+#define USE_RFKILL(...) __VA_ARGS__
+#define USE_TUNCTL(...) __VA_ARGS__
+#define USE_DMESG(...) __VA_ARGS__
+#define USE_HOSTNAME(...) __VA_ARGS__
+#define USE_KILLALL(...) __VA_ARGS__
+#define USE_MD5SUM(...) __VA_ARGS__
+#define USE_SHA1SUM(...) __VA_ARGS__
+#define USE_SHA224SUM(...)
+#define USE_SHA256SUM(...)
+#define USE_SHA384SUM(...)
+#define USE_SHA512SUM(...)
+#define USE_MKNOD(...) __VA_ARGS__
+#define USE_MKNOD_Z(...)
+#define USE_MKTEMP(...) __VA_ARGS__
+#define USE_MOUNT(...) __VA_ARGS__
+#define USE_PASSWD(...) __VA_ARGS__
+#define USE_PASSWD_SAD(...)
+#define USE_PIDOF(...) __VA_ARGS__
+#define USE_SEQ(...) __VA_ARGS__
+#define USE_SU(...) __VA_ARGS__
+#define USE_SYNC(...) __VA_ARGS__
+#define USE_UMOUNT(...) __VA_ARGS__
+#define USE_HELLO(...)
+#define USE_HOSTID(...)
+#define USE_SKELETON(...)
+#define USE_SKELETON_ALIAS(...)
+#define USE_TEST_HUMAN_READABLE(...)
+#define USE_TEST_MANY_OPTIONS(...)
+#define USE_TEST_SCANKEY(...)
+#define USE_TEST_UTF8TOWC(...)
+#define USE_GETENFORCE(...)
+#define USE_GETPROP(...)
+#define USE_LOAD_POLICY(...)
+#define USE_LOG(...)
+#define USE_RESTORECON(...)
+#define USE_RUNCON(...)
+#define USE_SENDEVENT(...)
+#define USE_SETENFORCE(...)
+#define USE_SETPROP(...)
+#define USE_START(...)
+#define USE_STOP(...)
+#define USE_TOYBOX(...) __VA_ARGS__
+#define USE_TOYBOX_SUID(...) __VA_ARGS__
+#define USE_TOYBOX_LSM_NONE(...) __VA_ARGS__
+#define USE_TOYBOX_SELINUX(...)
+#define USE_TOYBOX_SMACK(...)
+#define USE_TOYBOX_LIBCRYPTO(...)
+#define USE_TOYBOX_LIBZ(...)
+#define USE_TOYBOX_FLOAT(...) __VA_ARGS__
+#define USE_TOYBOX_HELP(...) __VA_ARGS__
+#define USE_TOYBOX_HELP_DASHDASH(...) __VA_ARGS__
+#define USE_TOYBOX_I18N(...) __VA_ARGS__
+#define USE_TOYBOX_FREE(...)
+#define USE_TOYBOX_NORECURSE(...)
+#define USE_TOYBOX_DEBUG(...)
+#define USE_TOYBOX_PEDANTIC_ARGS(...)
+#define USE_TOYBOX_MUSL_NOMMU_IS_BROKEN(...)
