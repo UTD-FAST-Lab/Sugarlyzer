b5ec9d574667c1ae08f52eca3b9f28b123297a45
0men1 <homahomen2@gmail.com>
2025-12-02 20:47:32 +0000

    unplug intercept script injections from programs

--- resources/programs/axtls/axtlsScript.sh
+++ resources/programs/axtls/axtlsScript.sh
@@ -8,7 +8,7 @@ apt-get update -y && apt-get install -y curl
 curl -L https://sourceforge.net/projects/axtls/files/2.1.4/axTLS-2.1.4.tar.gz/download --output axtls.tar.gz
 tar -xf axtls.tar.gz
 cd axtls-code
-make linuxconf CC="/Sugarlyzer/resources/tools/$TOOL/run$TOOL.sh"
+make linuxconf
 cp /SugarlyzerConfig/axtlsconfig.h config/config.h
 cd $CURDIR
 
--- resources/programs/toybox/toyboxScript.sh
+++ resources/programs/toybox/toyboxScript.sh
@@ -9,9 +9,7 @@ git clone https://github.com/landley/toybox.git
 cd toybox/
 git checkout 78289203031afc23585035c362beec10db54958d
 make defconfig
-
-make CC="/Sugarlyzer/resources/tools/$TOOL/run$TOOL.sh"
-
+make
 cp /SugarlyzerConfig/toyboxConfig.h generated/config.h
 cd $CURDIR
 
