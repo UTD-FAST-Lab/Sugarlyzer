8b2555ace7a389a566f491bf914d2e30521fe097
0men1 <homahomen2@gmail.com>
2025-12-09 19:29:36 +0000

    updated exp runner configurability

--- runBaselines.sh
+++ runBaselines.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
-if [ -z "$1" ]; then
+
+if [ -z "${1:-}" ]; then
   echo "Error: first parameter should be number of jobs."
   exit 1
 fi
 
-if [ -z "$2" ]; then
-  echo "Warning: putting results in current directory."
-  ROOT="."
-else
-  ROOT="$2"
+if [ -z "${2:-}" ]; then
+  echo "Error: second parameter should be number of samples."
+  exit 1
 fi
 
-RESULTS_DIR=$ROOT/experiment_results/product-based
+JOBS="$1"
+ROOT="${3:-.}"
+RESULTS_DIR="$ROOT/experiment_results/product-based"
 
-for t in infer clang phasar
-do
-  for p in axtls toybox busybox
-  do
-    for i in $(seq 1 5)
-    do
-      echo "Running $t on $p #$i\n"
-      dispatcher -t $t -p $p -r "$RESULTS_DIR/$t/$p/"$i"_results.json" --log "$RESULTS_DIR/$t/$p/"$i".log" --jobs $1 --baselines
+TOOLS=(infer clang phasar)
+PROJECTS=(axtls toybox busybox)
+ITERATIONS=5
+
+for t in "${TOOLS[@]}"; do
+  for p in "${PROJECTS[@]}"; do
+    BASE_DIR="$RESULTS_DIR/$t/$p"
+
+    for i in $(seq 1 "$ITERATIONS"); do
+      printf "Running %s on %s #%d\n" "$t" "$p" "$i"
+      ITERATION_DIR="$BASE_DIR/run_$i"
+
+      mkdir -p "$ITERATION_DIR"
+
+      dispatcher \
+        -t "$t" \
+        -p "$p" \
+        --output-dir "$ITERATION_DIR" \
+        -r "$ITERATION_DIR/results.json" \
+        --log "$ITERATION_DIR/run.log" \
+        --jobs "$JOBS" \
+        --sample-size "$2" \
+        --baselines
     done
   done
 done
--- runDesugared.sh
+++ runDesugared.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-if [ -z "$1" ]; then
+
+if [ -z "${1:-}" ]; then
   echo "Error: first parameter should be number of jobs."
   exit 1
 fi
 
-if [ -z "$2" ]; then
-  echo "Warning: putting results in current directory."
-  ROOT="."
-else
-  ROOT="$2"
-fi
+JOBS="$1"
+ROOT="${2:-.}"
+RESULTS_DIR="$ROOT/experiment_results/transformation-based"
 
-RESULTS_DIR=$ROOT/experiment_results/transformation-based
+TOOLS=(infer clang phasar)
+PROJECTS=(axtls toybox busybox)
+ITERATIONS=5
 
-for t in infer clang phasar
-do
-  for p in axtls toybox busybox
-  do
-    for i in $(seq 1 5)
-    do 
-      echo "Running $t on $p #$i\n"
-      dispatcher -t $t -p $p -r "$RESULTS_DIR/$t/$p/"$i"_results.json" --log "$RESULTS_DIR/$t/$p/"$i".log" --jobs $1
+for t in "${TOOLS[@]}"; do
+  for p in "${PROJECTS[@]}"; do
+    BASE_DIR="$RESULTS_DIR/$t/$p"
+
+    for i in $(seq 1 "$ITERATIONS"); do
+      printf "Running %s on %s #%d\n" "$t" "$p" "$i"
+      ITERATION_DIR="$BASE_DIR/run_$i"
+
+      mkdir -p "$ITERATION_DIR"
+
+      dispatcher \
+        -t "$t" \
+        -p "$p" \
+        --output-dir "$ITERATION_DIR" \
+        -r "$ITERATION_DIR/results.json" \
+        --log "$ITERATION_DIR/run.log" \
+        --jobs "$JOBS" 
     done
   done
 done
+
