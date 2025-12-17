1f3c5ec6cf8b52b3390f2846c50570c7a85749cf
0men1 <homahomen2@gmail.com>
2025-12-17 18:39:10 +0000

    Added timer to running family-based

--- runFamily.sh
+++ runFamily.sh
@@ -12,14 +12,16 @@ RESULTS_DIR="$ROOT/experiment_results/family-based"
 PROGRAMS=(axtls toybox busybox varbugs)
 
 for p in "${PROGRAMS[@]}"; do
-
   SCRIPT_PATH=$(find $(pwd) -type f -name "runVAA_$p.sh" -print -quit)
 
+
   BASE_DIR="$RESULTS_DIR/$p"
-  ITERATION_DIR="$BASE_DIR/run_$i"
-  mkdir -p "$ITERATION_DIR"
+  mkdir -p "$BASE_DIR"
+
+  TIMER_LOG="$BASE_DIR/timer.log"
 
   printf "Analyzing PROGRAM: %s\n" "$p"
 
-  bash "$SCRIPT_PATH" "$(realpath "$TYPECHEF_PATH")" "$(realpath "$ITERATION_DIR")"
+  /usr/bin/time -a -o "$TIMER_LOG" --format="$p: Real %e, User %U, Sys %S, MaxRSS %MKB" \
+  bash "$SCRIPT_PATH" "$(realpath "$TYPECHEF_PATH")" "$(realpath "$BASE_DIR")"
 done
