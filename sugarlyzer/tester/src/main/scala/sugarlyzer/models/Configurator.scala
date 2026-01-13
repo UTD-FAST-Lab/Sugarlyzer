9dac6597fd5c65c0732564b7d825f8b18cb8f648
0men1 <homahomen2@gmail.com>
2026-01-13 23:38:32 +0000

    applying config samples

--- sugarlyzer/tester/src/main/scala/sugarlyzer/models/Configurator.scala
+++ sugarlyzer/tester/src/main/scala/sugarlyzer/models/Configurator.scala
@@ -3,7 +3,6 @@ package sugarlyzer.models
 import cats.effect.{IO}
 import os._
 
-
 object Configurator {
   def stageAndConfigure(spec: ProgramSpecification): IO[Path] = IO.blocking {
     val targetRootDir = os.Path("/targets")
@@ -23,13 +22,15 @@ object Configurator {
       os.proc("tar", "-xf", tarDest, "-C", targetRootDir).call(check = false)
     if (proc.exitCode != 0) throw new RuntimeException("Tar extraction failed")
 
-    os.proc("bash", "-c", spec.buildCommand).call(
-      cwd = os.Path(
-        spec.targetDir
-      ),
-      stdout = os.Inherit,
-      stderr = os.Inherit
+    val buildProc = os.proc("bash", "-c", spec.buildCommand).call(
+      cwd = os.Path(spec.targetDir)
     )
+
+    if (buildProc.exitCode != 0) {
+      throw new RuntimeException(
+        s"Failed to run build command: ${spec.buildCommand}"
+      )
+    }
 
     os.Path(spec.targetDir)
   }
--- sugarlyzer/tester/src/main/scala/sugarlyzer/tester/tools/InferTool.scala
+++ sugarlyzer/tester/src/main/scala/sugarlyzer/tester/tools/InferTool.scala
@@ -3,6 +3,7 @@ package sugarlyzer.tester.tools
 import sugarlyzer.models.ProgramSpecification
 
 import cats.effect.{IO}
+import os._
 
 object InferTool extends AnalysisTool {
   def name(): String = { "Infer" }
@@ -13,7 +14,26 @@ object InferTool extends AnalysisTool {
       _ <- IO.println(s"Program build command: ${spec.buildCommand}")
       _ <- IO.println(s"Program target dir: ${spec.targetDir}")
 
+      _ <- applyConfiguration(spec)
+
     } yield ("End")
+  }
+
+  def applyConfiguration(spec: ProgramSpecification): IO[Unit] = IO.blocking {
+    println("Applying configuration")
+
+    val proc = os.proc("bash", "-c", spec.buildCommand).call(
+      cwd = os.Path(spec.targetDir)
+    )
+
+    println("Program configuration done")
+
+    if (proc.exitCode != 0) {
+      throw new RuntimeException(
+        s"Failed to run build command: ${spec.buildCommand}"
+      )
+    }
+
   }
   def parseOutput(rawOutput: String): List[String] = {
     List("O")
