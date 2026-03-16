f033a19cd6d056176d06f804b16c4b2b80f53a9d
Austin Mordahl <amordahl@uic.edu>
2026-03-16 17:59:56 -0500

    update from icst

--- scripts/add_classification_field.py
+++ scripts/add_classification_field.py
@@ -6,29 +6,23 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 p = argparse.ArgumentParser()
-p.add_argument("csv", help="The CSV that contains the classifications")
+p.add_argument("json", help="The JSON file that contains the classifications")
 args = p.parse_args()
 
 results = json.loads(sys.stdin.read().strip())
 
-if args.csv is None:
+if args.json is None:
     for r in results:
         r['classification'] = ''
 else:
-   # Read the CSV file and create a mapping from id to classification
-   import csv
-   classification_map = {}
-   with open(args.csv, newline='') as csvfile:
-       reader = csv.DictReader(csvfile)
-       for row in reader:
-           classification_map[row['ID']] = row['Final Classification']
-
-   for r in results:
-       if r['id'] in classification_map:
-           r['classification'] = classification_map.get(r['id'], '')
-       else:
-           logging.info("CSV did not contain id for %s", r['id'])
-           r['classification'] = ''
+   # Read the JSON file
+    import json
+    with open(args.json, 'r') as f:
+        classification_data = json.load(f)
+    for r in results:
+        for c in classification_data:
+            if all(k in r and k in c and r[k] == c[k] for k in c.keys() if k != 'classification'):
+                r['classification'] = c['classification']
 
 try:
   print(json.dumps(results, indent=2))
