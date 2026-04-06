461c157bd78ce11bbe4d4efcf46d0ae70ecf91ee
Austin Mordahl <amordahl@uic.edu>
2026-04-06 13:09:39 -0500

    better names

--- sugarlyzer/tester/src/main/scala/sugarlyzer/tester/tools/AnalysisTool.scala
+++ sugarlyzer/tester/src/main/scala/sugarlyzer/tester/tools/AnalysisTool.scala
@@ -16,20 +16,20 @@ case class ToolAlarm(
 )
 
 sealed trait StrategyAlarm {
-  def finding: ToolAlarm
+  def originalAlarm: ToolAlarm
 }
 
 case class ProductAlarm(
-    finding: ToolAlarm,
+    originalAlarm: ToolAlarm,
     configFiles: List[String],
     model: List[(String, String)],
     numConfigs: List[Int]
 ) extends StrategyAlarm
 
 case class TransformationAlarm(
-    finding: ToolAlarm,
+    originalAlarm: ToolAlarm,
     sanitizedDescription: String,
-    lineInputFile: (Int, Int),
+    lineInputFile: Option[(Int, Int)],
     presenceCondition: PresenceCondition,
     model: String,
     feasible: Boolean,
