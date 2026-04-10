7412fa33812265e50b6a2c73bcf56422a35033b2
Austin Mordahl <amordahl@uic.edu>
2026-04-10 12:59:22 -0500

    update with leak

--- sugarlyzer/tester_test/resources/sample1.c
+++ sugarlyzer/tester_test/resources/sample1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  #ifdef HELLO_WORLD
-  #if SECOND_NESTING
-  #ifndef THIRD_NESTING
+int main()
+{
+#ifdef HELLO_WORLD
+#if SECOND_NESTING
+#ifndef THIRD_NESTING
+  int *leak = malloc(sizeof(int));
   printf("Hello, World!\n");
-  #endif
-  #endif
+#endif
+#endif
   return 0;
-  #else
+#else
   printf("Goodbye, World!\n");
   return -1;
-  #endif
+#endif
 }
