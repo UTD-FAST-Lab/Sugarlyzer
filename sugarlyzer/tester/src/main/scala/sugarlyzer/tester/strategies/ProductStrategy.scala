399281ad601b0fe5e5754cd7f2f9a2ae804c4855
0men1 <homahomen2@gmail.com>
2026-05-11 06:48:08 +0000

    patch memory leaks

--- sugarlyzer/tester/src/main/scala/sugarlyzer/tester/strategies/ProductStrategy.scala
+++ sugarlyzer/tester/src/main/scala/sugarlyzer/tester/strategies/ProductStrategy.scala
@@ -32,7 +32,6 @@ object ProductStrategy extends AnalysisStrategy {
       spec: ProgramSpecification,
       tool: AnalysisTool
   ): IO[List[ProductAlarm]] = {
-    given AppConfig = appConfig
     println(s"Running analysis for ${spec.name}")
     if spec.name == "varbugs" then {
       val sharedPath = os.Path(appConfig.sharedPath)
